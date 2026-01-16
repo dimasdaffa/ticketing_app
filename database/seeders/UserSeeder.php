d359277729692e134da34674f1b9dbe1f61b517b
dimasdaffa <dimas.de2004@gmail.com>
2026-01-16 13:20:24 +0700

    Add Blade components and authentication routes for user management
    - Created various Blade components for dropdowns, buttons, inputs, and modals to enhance UI consistency.
    - Implemented user authentication routes including registration, login, password reset, and email verification.
    - Developed profile management features allowing users to update their information and delete their accounts.
    - Added tests for authentication, email verification, password confirmation, and profile management to ensure functionality.
    - Configured Tailwind CSS for styling and responsiveness across components.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@mail.com',
+                'password' => bcrypt('admin'),
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Regular User',
+                'email' => 'user@mail.com',
+                'password' => bcrypt('user'),
+                'no_hp' => '081234567890',
+                'role' => 'user',
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
